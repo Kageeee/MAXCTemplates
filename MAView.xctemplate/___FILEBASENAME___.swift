ae4661b0f1d0819558e1a5726b82d66043912d80
Miguel Alcântara <miguel.alcantara@innowave.tech>
2019-06-12 12:13:06 +0100

    added templates

---
+++ MAView.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,58 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+import Foundation
+
+// MARK: - Protocols
+
+class ___VARIABLE_productName___: UIView {
+    
+    // MARK: - Properties
+    
+    // MARK: - Subviews
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    
+    // MARK: - Functions
+    
+    func setup() {
+        setupViews()
+        setupConstraints()
+    }
+    
+    func setupViews() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+    
+    
+    // MARK: - Private helper functions
+    
+}
+
+
+// MARK: - Extensions
+
+extension ___VARIABLE_productName___ {
+    
+}
+
